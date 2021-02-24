c44fd7d77dc037a3dcb5fbae633f85a58d8a484b
wes <Wesleymiller1996@gmail.com>
2021-02-23 21:31:53 -0600

    deleted branch

---
+++ ConvertMarkdown/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertMarkdown
+{
+    public class Parser
+    {
+        public Parser()
+        {
+
+        }
+
+        public string Parse(MarkdownToken token)
+        {
+            string test = token.TokenMatch.Value;
+            Console.WriteLine(test);
+            return "";
+        }
+    }
+}
